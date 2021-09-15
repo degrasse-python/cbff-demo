83b9249d6225f1344d8ce4ea3a83dbd2f9c8b1fa
Deon 'PrimeTime' Saunders <adsaunde@protonmail.com>
2021-09-14 20:18:06 -0400

    db change

--- core/settings.py
+++ core/settings.py
@@ -125,7 +125,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'djongo',
         'NAME': ENV('MONGO_DBNAME'),
@@ -150,6 +150,14 @@ DATABASES = {
             },
             },
     }
+}"""
+
+
+DATABASES = {
+  'default': {
+    'ENGINE': 'django.db.backends.sqlite3',
+    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+  }
 }
 
 # Password validation
