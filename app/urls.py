18e8ae4042627ae72ef613f93c10bb3207f9925c
Deon 'PrimeTime' Saunders <adsaunde@protonmail.com>
2021-10-26 01:14:03 -0400

    fixes

--- app/urls.py
+++ app/urls.py
@@ -10,21 +10,18 @@ from authentication.views import login_view
 
 
 urlpatterns = [
+  # The dashboard page
+  path('', views.dashboard, name='dashboard'),
 
-    # The dashboard page
-    path('', views.pages, name='pages'
-    ),
+  # Matches any html file
+  re_path(r'^.*\.*', views.pages, name='pages'),
 
-    # Matches any html file
-    re_path(r'^.*\.*', views.pages, name='pages'),
+  # API paths
+  path('get_data', views.get_data, name='get_data'),
 
-    # API paths
-    path('get_data', views.get_data, name='get_data'),
+  # load data 
+  path('pivot_data', views.pivot_data, name='pivot_data'),
 
-    # load data 
-    path('pivot_data', views.pivot_data, name='pivot_data'),
-
-    # graphs
-    path('line_chart', views.line_chart, name='line_chart'),
-
+  # graphs
+  path('line_chart', views.line_chart, name='line_chart'),
 ]
--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# exit on error
-set -o errexit
-
-poetry install
-
-python3 manage.py collectstatic --no-input
-python3 manage.py migrate
